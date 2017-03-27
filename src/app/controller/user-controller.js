5ab08069d1adf1a76465da083b7f4c1ce4bef066
Christian Auer <auer.christian4000@googlemail.com>
2017-03-27 21:53:44 +0200

    extracted user validation

--- src/app/controller/user-controller.js
+++ src/app/controller/user-controller.js
@@ -1,43 +1,14 @@
 const Task = require('data.task')
-const Validation = require('data.validation')
+const {validUser} = require('../validation/user-validation')
 const {Success, Failure} = require('data.validation')
 const {Right, Left} = require('data.either')
 const {Just, Nothing} = require('data.maybe')
-const {curry, prop, compose, assoc} = require('ramda')
+const {curry, curryN, identity, prop, compose, assoc} = require('ramda')
 const {hash, compare} = require('../crypto')
 const {isEqual, match, minLength, maxLength, taskFromValidation} = require('../validation')
 const validationError = require('../error/validationError')
 const notFoundError = require('../error/notFoundError')
 const emailError = require('../error/emailError')
-
-const emailRegEx = /^[\w\.]+@[a-zA-Z_-]+?\.[a-zA-Z]{2,10}$/g
-const passwordRegEx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,16}$/g
-
-const user = curry((name, email, password) => ({name, email, password}))
-
-const validatePassword = (verification, password) =>
-  Validation.of(curry((a,b,c,d) => a))
-  .ap(isEqual('Password and verification must be equal.', verification, password))
-  .ap(minLength(8, 'Password must be longer than 4 characters.', password))
-  .ap(maxLength(40, 'Username should not be longer than 40 characters', password))
-  .ap(match(passwordRegEx, 'Password of invalid format.', password))
-
-const validateName = (name) =>
-  Validation.of(curry((a,b) => a))
-  .ap(minLength(4, 'Username must be longer than 4 characters.', name))
-  .ap(maxLength(40, 'Username should not be longer than 40 characters', name))
-
-const validateEmail = (email) =>
-  Validation.of(curry((a,b,c) => a))
-  .ap(match(emailRegEx, 'E-Mail of invalid fromat.', email))
-  .ap(minLength(6, 'E-Mail must be longer than 6 characters.', email))
-  .ap(maxLength(60, 'E-Mail should not be longer than 60 characters.', email))
-
-const validUser = ({name, email, password, verification}) =>
-  Validation.of(user)
-  .ap(validateName(name))
-  .ap(validateEmail(email))
-  .ap(validatePassword(verification, password))
 
 const sendConfirmationEMail = curry((db, email, userEmail, template) =>
   email.send('Tabata Confirmation', userEmail, template)
@@ -66,132 +37,145 @@ const isVerified = (user) =>
 
 const User = ({db, email}) => {
   // AUTHENTIFICATION MIDDLEWARE
+  /**
+   * Extracts the user id from the user and calles the done callback with it.
+   * @param {Object} user
+   * @param {String} user._id
+   * @param {Function} done
+   */
   const serialize = (user, done) => done(null, user._id)
-  const deserialize = (id, done) => {
-    return db.findById('User', id)
-    .fork(done, maybeUser => maybeUser.cata({
-      Just: (user) => done(null, user),
-      Nothing: ()  => done(null, null)
-    }))
-  }
+  /**
+   * Finds an user by id and calles the done callback with it.
+   * @param {String} id - The id of an user.
+   * @param {Function} done - callback.
+   */
+  const deserialize = (id, done) =>
+    db.findById('User', id)
+      .fork(done, maybeUser => maybeUser.cata({
+        Just: (user) => done(null, user),
+        Nothing: (_) => done(null, null)
+      }))
+  /**
+   * Authenticates an user.
+   * @param {String} email - The e-mail of an persisted user.
+   * @param {String} password - The password of the user with the e-mail.
+   * @param {Function} done - Callback
+   */
   const authenticate = (email, password, done) =>
     db.findOne('User', {email: email})
-    .chain(maybeUser => maybeUser.cata({
-      Just: compose(Task.of, isVerified),
-      Nothing: () => Task.rejected('Incorrect username.')
-    }))
-    .chain(isVerified => isVerified.cata({
-      Success: comparePasswords(password),
-      Failure: () => Task.rejected('E-Mail is not verified.')
-    }))
-    .fork(
-      (error) => done(null, false, {message: error}),
-      (user) => done(null, user)
-    )
-  // )
-
+      .chain(maybeUser => maybeUser.cata({
+        Just: compose(Task.of, isVerified),
+        Nothing: () => Task.rejected('Incorrect username.')
+      }))
+      .chain(isVerified => isVerified.cata({
+        Success: comparePasswords(password),
+        Failure: () => Task.rejected('E-Mail is not verified.')
+      }))
+      .fork(
+        (error) => done(null, false, {message: error}),
+        (user) => done(null, user)
+      )
   /**
    * Writes a valid user into a database where email must be unique.
    * @Side-Effect:
    *    Writing to database
    *    Reading from filesystem
    *    Sending an E-Mail
-   * @param {Object} user - {
-   *  {String} name,
-   *  {String} email,
-   *  {String} password,
-   *  {String} verification
-   * }
+   * @param {Object} user
+   * @param {String} user.name
+   * @param {String} user.email
+   * @param {String} user.password
+   * @param {String} user.verification
    * @return {Task} Array({String} Error) User
    */
-  const registration = (user) => {
-    return taskFromValidation(validUser(user))
-    .chain(hash('password'))
-    .chain(db.createUnique('email', 'User'))
-    .map(assoc('url', 'http://localhost:3000/v1/user/confirm/'))
-    .chain(email.renderEmail('confirmation-email'))
-    .map(prop('html'))
-    .chain(sendConfirmationEMail(db, email, user.email))
-  }
+  const registration = (user) =>
+    taskFromValidation(validUser(user))
+      .chain(hash('password'))
+      .chain(db.createUnique('email', 'User'))
+      .map(assoc('url', 'http://localhost:3000/v1/user/confirm/'))
+      .chain(email.renderEmail('confirmation-email'))
+      .map(prop('html'))
+      .chain(sendConfirmationEMail(db, email, user.email))
   /**
    * Sets a users verified property to true.
    * @Side-Effect:
    *    Writing to database
    * @param {String} id - the user id.
-   * @return {Task} NotFoundError User
+   * @return {Task} Task NotFoundError User
    */
-  const confirmation = (id) => {
-    return db.updateById('User', {verified: true}, id)
-    .chain(maybeUserToTask)
-  }
+  const confirmation = (id) =>
+    db.updateById('User', {verified: true}, id)
+      .chain(maybeUserToTask)
   /**
    * Sets a token on which the user can reset the password and sends an E-Mail.
    * @Side-Effect:
    *    Writing to database
    *    Reading from filesystem
    *    Sending an E-Mail
-   * @param {Object} user - {
-   *  {String} email,
-   * }
-   * @return {Task} Error User
+   * @param {Object} user
+   * @param {String} user.email
+   * @return {Task} Task Error User
    */
-  const sendResetPasswordEmail = (user) => {
-    return Task.of(curry((token, id) => Object.assign({}, token, {id: id})))
-    .ap(hash('token', {token: user.email}))
-    .ap(db.findOne('User', {email: user.email})
+  const sendResetPasswordEmail = (user) =>
+    Task.of(curry((token, id) => Object.assign({}, token, {id: id})))
+      .ap(hash('token', {token: user.email}))
+      .ap(db
+        .findOne('User', {email: user.email})
+        .chain(maybeUserToTask)
+        .map(prop('_id'))
+      )
+      .chain(({id, token}) => db.updateById('User', {token: token}, id))
       .chain(maybeUserToTask)
-      .map(prop('_id'))
-    )
-    .chain(({id, token}) => db.updateById('User', {token: token}, id))
-    .chain(maybeUserToTask)
-    .map(assoc('url', 'http://localhost:3000/v1/user/resetPassword/'))
-    .chain(email.renderEmail('reset-password-email'))
-    .map(prop('html'))
-    .chain(email.send('Tabata Reset Password', user.email))
-  }
-  const resetPasswordPage = (token) => {
-    return db.findOne('User', {token: token})
-    .chain(maybeUserToTask)
-  }
+      .map(assoc('url', 'http://localhost:3000/v1/user/resetPassword/'))
+      .chain(email.renderEmail('reset-password-email'))
+      .map(prop('html'))
+      .chain(email.send('Tabata Reset Password', user.email))
+  /**
+   * Finds a user by a token if both, the token and the user exists.
+   * @param {String} token - The token by which the user will be searched.
+   * @return {Task} Task Error User
+   */
+  const resetPasswordPage = (token) =>
+    db.findOne('User', {token: token})
+      .chain(maybeUserToTask)
   /**
    * Updates a users password and sends an email to verify to the user.
    * @Side-Effect:
    *    Writing to database
    *    Reading from filesystem
    *    Sending an E-Mail
-   * @param {Object} user - {
-   *  {String} _id,
-   *  {String} password,
-   *  {String} verification
-   * }
-   * @return {Task} Error User
+   * @param {Object} user
+   * @param {String} user._id
+   * @param {String} user.password
+   * @param {String} user.verification
+   * @return {Task} Task Error User
    */
-  const resetPassword = (user) => {
-    return validatePassword(user.verification, user.password)
-    .cata({
-      Success: (password) => db.findById('User', user._id),
-      Failure: (error) => Task.rejected(error)
-    })
-    .chain(maybeUserToTask)
-    .map(user => user.token ? Just(user) : Nothing())
-    .chain(maybeUser => maybeUser.cata({
-      Just: Task.of,
-      Nothing: () => Task.rejected({status: 401})
-    }))
-    .chain(savedUser => hash('password', {password: user.password})
-      .map(({password}) => Object.assign({}, savedUser._doc, {
-        token: null,
-        password: password,
-        verified: false
+  const resetPassword = (user) =>
+    validatePassword(user.verification, user.password)
+      .cata({
+        Success: (password) => db.findById('User', user._id),
+        Failure: (error) => Task.rejected(error)
+      })
+      .chain(maybeUserToTask)
+      .map(user => user.token ? Just(user) : Nothing())
+      .chain(maybeUser => maybeUser.cata({
+        Just: Task.of,
+        Nothing: () => Task.rejected({status: 401})
       }))
-    )
-    .chain(user => db.updateById('User', user, user._id))
-    .chain(maybeUserToTask)
-    .map(assoc('url', 'http://localhost:3000/v1/user/confirm/'))
-    .chain(email.renderEmail('confirmation-email'))
-    .map(prop('html'))
-    .chain(sendConfirmationEMail(db, email, user.email))
-  }
+      .chain(savedUser => hash('password', {password: user.password})
+        .map(({password}) => Object.assign({}, savedUser._doc, {
+          token: null,
+          password: password,
+          verified: false
+        }))
+      )
+      .chain(user => db.updateById('User', user, user._id))
+      .chain(maybeUserToTask)
+      .map(assoc('url', 'http://localhost:3000/v1/user/confirm/'))
+      .chain(email.renderEmail('confirmation-email'))
+      .map(prop('html'))
+      .chain(sendConfirmationEMail(db, email, user.email))
+
   return {
     serialize,
     deserialize,
@@ -203,7 +187,5 @@ const User = ({db, email}) => {
     resetPassword,
   }
 }
-
-
 
 module.exports = User
---
+++ src/app/validation/user-validation.js
@@ -0,0 +1,38 @@
+const Validation = require('data.validation')
+const { curry, curryN, identity } = require('ramda')
+const {isEqual, match, minLength, maxLength, taskFromValidation} = require('./index')
+const emailRegEx = /^[\w\.]+@[a-zA-Z_-]+?\.[a-zA-Z]{1,10}$/g
+const passwordRegEx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*/g
+
+const user = curry((name, email, password) => ({name, email, password}))
+
+const validatePassword = (verification, password) =>
+  Validation.of(curryN(4, identity))
+  .ap(isEqual('Password and verification must be equal.', verification, password))
+  .ap(minLength(8, 'Password must be longer than 8 characters.', password))
+  .ap(maxLength(40, 'Password should not be longer than 40 characters.', password))
+  .ap(match(passwordRegEx, 'Password of invalid format.', password))
+
+const validateName = (name) =>
+  Validation.of(curryN(2, identity))
+  .ap(minLength(4, 'Username must be longer than 4 characters.', name))
+  .ap(maxLength(40, 'Username should not be longer than 40 characters', name))
+
+const validateEmail = (email) =>
+  Validation.of(curryN(3, identity))
+  .ap(match(emailRegEx, 'E-Mail of invalid format.', email))
+  .ap(minLength(6, 'E-Mail must be longer than 6 characters.', email))
+  .ap(maxLength(60, 'E-Mail should not be longer than 60 characters.', email))
+
+const validUser = ({name, email, password, verification}) =>
+  Validation.of(user)
+  .ap(validateName(name))
+  .ap(validateEmail(email))
+  .ap(validatePassword(verification, password))
+
+module.exports = {
+  validUser,
+  validatePassword,
+  validateName,
+  validateEmail
+}
